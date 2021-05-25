6ab6b32fba8a2fd32e8a30873261cc9f3b6135b0
Leslie <leslie.liszcz@gmail.com>
2021-05-25 11:37:17 +0100

    Refactored code displaying total number of students.

--- directory.rb
+++ directory.rb
@@ -16,6 +16,6 @@ puts "Norman Bates"
 
 # finally, we print the total number of students
 
-print "Overall, we have "
-print 11
-puts " great students."
+student_count = 11
+
+puts "Overall, we have #{student_count} great students."
