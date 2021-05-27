52be4645e381d06fae0c973eee7e7229b6d126cb
Leslie <leslie.liszcz@gmail.com>
2021-05-27 13:37:05 +0100

    Enabled users to submit names.

--- directory.rb
+++ directory.rb
@@ -1,22 +1,27 @@
-# put students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+puts "Please enter the names of the students: "
+puts "To finish, just hit return twice."
+
+# create and empty array
+students = []
+
+# get the first name
+name = gets.chomp
+
+# while the name is not empty, repeat this code
+while !name.empty? do
+  # add name to array
+  students << { name: name, cohort: :november }
+  puts "Now we have #{students.count} students."
+  # get another name from the user
+  name = gets.chomp
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+#return array of students
+students
 
 def print(students)
   students.each do |student|
@@ -28,7 +33,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
 
-# nothing happens until we call each method
+
 print_header
 print(students)
 print_footer(students)
