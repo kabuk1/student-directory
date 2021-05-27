4d92619b4e6963e1ec965afddacc274f4a97128e
Leslie <leslie.liszcz@gmail.com>
2021-05-27 15:09:58 +0100

    Added method to handle student input.

--- directory.rb
+++ directory.rb
@@ -1,31 +1,27 @@
-puts "Please enter the names of the students: "
-puts "To finish, just hit return twice."
-
-# create and empty array
-students = []
-
-# get the first name
-name = gets.chomp
-
-# while the name is not empty, repeat this code
-while !name.empty? do
-  # add name to array
-  students << { name: name, cohort: :november }
-  puts "Now we have #{students.count} students."
-  # get another name from the user
+def input_students
+  puts "Please enter the names of the students: "
+  puts "To finish, just hit return twice."
+  students = [] 
   name = gets.chomp
+
+  while !name.empty? do
+    # add name to array
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students."
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
 end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-#return array of students
-students
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohrot)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -33,7 +29,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
 
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
