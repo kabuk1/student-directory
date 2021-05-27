3143b234237368aaf46e76e1f8f0f1f53dfb18d3
Leslie <leslie.liszcz@gmail.com>
2021-05-27 12:12:36 +0100

    Refactored to use methods.

--- directory.rb
+++ directory.rb
@@ -13,12 +13,22 @@ students = [
   "Norman Bates"
 ]
 
-# prints each student in the array
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-# finally, we print the total
-puts "Overall, we have #{students.count} great students."
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students."
+end
+
+# nothing happens until we call each method
+print_header
+print(students)
+print_footer(students)
